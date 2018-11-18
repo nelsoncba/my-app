6e6f53194121e48e26ce1ee49a8a2942e60e1871
vagrant <vagrant@homestead>
2018-11-18 21:36:01 +0000

    Authenticate with username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- authentication with username -->
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
